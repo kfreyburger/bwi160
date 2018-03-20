aec725c5c7bb2251737a42dbf33b1ca502885e74
Klaus Freyburger <Klaus Freyburger@DESKTOP-7IB76VI.fritz.box>
2018-03-20 08:12:16 +0100

    Demo

---
+++ bw140SoSe18/src/de/hslu/bw140/v04aAnweisungenTeil1/DemoAnweisungen.java
@@ -0,0 +1,10 @@
+package de.hslu.bw140.v04aAnweisungenTeil1;
+
+public class DemoAnweisungen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hallo");
+	}
+
+}
