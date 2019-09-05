a848990148bb2ccd51933960434c9902e4f5a9b2
Klaus Freyburger <klaus.freyburger@hs-lu.de>
2019-09-05 12:02:04 +0200

    Anzahl flexibel

--- bwi160/src/de/hslu/bwi160/v02/AlgorihmischeGrundkonzepte/P2Maximum.java
+++ bwi160/src/de/hslu/bwi160/v02/AlgorihmischeGrundkonzepte/P2Maximum.java
@@ -3,8 +3,8 @@ package de.hslu.bwi160.v02.AlgorihmischeGrundkonzepte;
 public class P2Maximum {
 	public static void main(String[] args) {
 		int n, i, merker;
-		n = 6;
 		int[] a = { 5, 1, 8, 3, 9, 2 };
+		n = a.length;
 		merker = a[0];
 		i = 1;
 		while (i < n) {
