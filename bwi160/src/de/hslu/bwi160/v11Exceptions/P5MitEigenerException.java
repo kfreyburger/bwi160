9264ef816368198f2d4d851b0f5316418e3cd64a
Freyburger <Freyburger@NBB305FREYB.CAMPUS.fh-ludwigshafen.de>
2023-05-23 20:53:03 +0200

    Exception Code aufräumen

--- bwi160/src/de/hslu/bwi160/v11Exceptions/P5MitEigenerException.java
+++ bwi160/src/de/hslu/bwi160/v11Exceptions/P5MitEigenerException.java
@@ -15,16 +15,16 @@ public class P5MitEigenerException {
 			System.out.println("Das Ergebnis ist " + divideByZeroThrows(a, b));
 
 		} catch (P4MeineEigeneException e) {
-//			System.out.println("DivideByZeroException gefangen");
-			System.out.println(e);
+			System.out.println("DivideByZeroException gefangen");
+//			System.out.println(e);
 //    Zur Abwechslung mal etwas ausführlicher:
-//			e.printStackTrace();
+			e.printStackTrace();
 		} // try
 	}// method
 
 	public static int divideByZeroThrows(int a, int b)
 			throws P4MeineEigeneException {
-//		int a = 1, b = 0;
+		
 		if (b == 0) {
 			P4MeineEigeneException d = new P4MeineEigeneException();
 			throw d;
