48098e13a6c1f30e45bec791273b2ff89be8da7a
Klaus Freyburger <klaus.freyburger@hs-lu.de>
2023-05-23 20:29:00 +0200

    File Input mit throws

--- bwi160/src/de/hslu/bwi160/v09Input/P5ScannerFile.java
+++ bwi160/src/de/hslu/bwi160/v09Input/P5ScannerFile.java
@@ -2,18 +2,17 @@ package de.hslu.bwi160.v09Input;
 
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class P5ScannerFile {
 
-	public static void main(String[] args) {
-		try {
+	public static void main(String[] args) throws FileNotFoundException {
+		
 			Scanner textFile = new Scanner(new File("input_text.txt"));
 			while (textFile.hasNextLine())
 				System.out.println(textFile.nextLine());
 			textFile.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		
 	}
 
 }
